97ae4521d07a21b4f50abbb728f0bb3a7d8edcbd
gate12s <nenedcjnef@gmail.com>
2022-06-10 16:56:46 +0400

    vtvirtav final project mangas

---
+++ js/chat.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("myForm").style.height = "460px";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.height = "100px";
+  }
+
+
